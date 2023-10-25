7fcfde8c71e0ffde80985b05b4394c3bb5809d7a
ibtissem <ibtissem.rezig@citalid.com>
2023-10-26 00:28:51 +0200

    fix player position

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { Vector3 } from 'three';
 
 export default {
-  initialPlayerPosition: new Vector3(0, 0, 20),
-  initialCameraPosition: new Vector3(0, 10, 34),
+  initialPlayerPosition: new Vector3(0, 0, 0),
+  initialCameraPosition: new Vector3(0, 10, 20),
 };
