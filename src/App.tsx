48a97efce7f2c641e942f48ad24689461e63c1e6
Julien <julien@goliaths.io>
2023-10-25 23:16:40 +0200

    remove overflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Scene } from './scene/Scene';
 
 export default function App() {
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
       <Menu>
         <Scene />
       </Menu>
