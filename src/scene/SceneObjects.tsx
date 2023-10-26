bf9a016818835687be573f05e0eddfd66aab5e09
Julien <julien@goliaths.io>
2023-10-26 03:13:28 +0200

    better color for eyes

--- src/scene/SceneObjects.tsx
+++ src/scene/SceneObjects.tsx
@@ -41,7 +41,7 @@ const Ball = () => {
     <group ref={ref} {...{ position: config.initialBallPosition }}>
       <Float speed={20} rotationIntensity={0.04} floatIntensity={2}>
         <Sphere args={[4]}>
-          <meshBasicMaterial color={'red'} />
+          <meshBasicMaterial color={'#888'} />
         </Sphere>
       </Float>
     </group>
