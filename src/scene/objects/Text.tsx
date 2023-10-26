1eb032ecfb5d771ff8816f93365f9f832d744576
ibtissem <ibtissem.rezig@citalid.com>
2023-10-26 19:46:25 +0200

    add text animation

--- src/scene/objects/Text.tsx
+++ src/scene/objects/Text.tsx
@@ -2,8 +2,10 @@
 
 import { extend, useLoader } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
+import { Mesh } from 'three';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { useSceneContext } from '../../context/scene-context';
 
 extend({ TextGeometry });
 
@@ -11,6 +13,12 @@ export const Text = () => {
   const font = useLoader(FontLoader, './src/assets/Roboto-Bold.json');
   const [_, setFontLoaded] = useState(false);
   const toggle = useRef(false);
+  const ref = useRef<Mesh>(null);
+  const { exposeObject } = useSceneContext();
+
+  useEffect(() => {
+    exposeObject('text', ref);
+  }, []);
 
   useEffect(() => {
     toggle.current = !toggle.current;
@@ -18,7 +26,7 @@ export const Text = () => {
   }, [font]);
 
   return (
-    <mesh castShadow {...{ position: [30, 5, -120] }}>
+    <mesh ref={ref} castShadow {...{ position: [30, 5, -120] }}>
       <textGeometry args={['test', { font, size: 4, height: 3 }]} />
       <meshLambertMaterial attach="material" color={'gold'} />
     </mesh>
