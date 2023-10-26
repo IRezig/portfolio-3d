c1bfde0187a3251914468ce111d7f5dafe139cd5
Julien <julien@goliaths.io>
2023-10-26 04:34:05 +0200

    loaded character

--- src/scene/objects/Player.tsx
+++ src/scene/objects/Player.tsx
@@ -1,6 +1,8 @@
-import { Float } from '@react-three/drei';
+/* eslint-disable */
+import { Float, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import { useEffect, useRef } from 'react';
-import { Group } from 'three';
+import { AnimationMixer, Group } from 'three';
 
 import config from '../../config/config';
 import { useSceneContext } from '../../context/scene-context';
@@ -15,6 +17,25 @@ export const Player = () => {
   useEffect(() => {
     exposeObject('player', ref);
   }, []);
+
+  const { scene, animations } = useGLTF('./src/character.gtlf');
+  const mixer = useRef<AnimationMixer | null>(null);
+
+  useFrame((state, delta) => {
+    mixer.current?.update(delta);
+  });
+
+  useEffect(() => {
+    /*mixer.current = new AnimationMixer(scene);
+    const action = mixer.current.clipAction(animations[0]);
+    action.play();*/
+  }, [animations, scene]);
+
+  return (
+    <group ref={ref} rotation={[0, Math.PI, 0]}>
+      <primitive object={scene} />
+    </group>
+  );
 
   return (
     <Float
