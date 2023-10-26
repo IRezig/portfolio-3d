bfd5a364edd7b985f8f0211b0e791353e849ed53
Julien <julien@goliaths.io>
2023-10-26 04:17:40 +0200

    ball is actually an atom

--- src/scene/objects/Ball.tsx
+++ src/scene/objects/Ball.tsx
@@ -1,9 +1,10 @@
-import { Float, Sphere } from '@react-three/drei';
+import { Float } from '@react-three/drei';
 import { useEffect, useRef } from 'react';
 import { Group } from 'three';
 
 import config from '../../config/config';
 import { useSceneContext } from '../../context/scene-context';
+import { Atom } from './Atom';
 
 export const Ball = () => {
   const ref = useRef<Group>(null);
@@ -16,9 +17,7 @@ export const Ball = () => {
   return (
     <group ref={ref} {...{ position: config.initialBallPosition }}>
       <Float speed={20} rotationIntensity={0.04} floatIntensity={2}>
-        <Sphere args={[4]}>
-          <meshBasicMaterial color={'#888'} />
-        </Sphere>
+        <Atom />
       </Float>
     </group>
   );
