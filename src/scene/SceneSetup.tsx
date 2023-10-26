26707375998cd821d09d068bf31404d11d5fd892
Julien <julien@goliaths.io>
2023-10-26 20:43:27 +0200

    spotlight not showing shadows

--- src/scene/SceneSetup.tsx
+++ src/scene/SceneSetup.tsx
@@ -1,14 +1,22 @@
 import { Environment } from '@react-three/drei';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
+import config from '../config/config';
+
+const angleToRad = (degrees: number) => {
+  return (Math.PI / 180) * degrees;
+};
+
 export const SceneSetup = () => {
   return (
     <>
       {/* Light */}
+      <ambientLight intensity={0.1} />
+
       <directionalLight
         castShadow
-        color={'#ffffff'}
         position={[-100, 20, -100]}
+        color="white"
         intensity={10}
         shadow-mapSize-width={10000}
         shadow-mapSize-height={10000}
@@ -17,6 +25,15 @@ export const SceneSetup = () => {
         shadow-camera-right={400}
         shadow-camera-top={400}
         shadow-camera-bottom={-400}
+      />
+      <spotLight
+        castShadow
+        position={[
+          config.objects.ball.initialPosition.x,
+          20,
+          config.objects.ball.initialPosition.z,
+        ]}
+        args={['red', 4000, 0, angleToRad(180), 0]}
       />
 
       {/* Background Color */}
